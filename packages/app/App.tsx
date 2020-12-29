676f0e34f1a12af9b358729c1fb5aa184c371d33
Michi Moesl <michael.moesl@me.com>
2020-12-29 18:03:02 +0100

    chore: remove old navigation

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -7,7 +7,6 @@ import "react-native-gesture-handler"
 import useCachedResources from "./hooks/useCachedResources"
 import useColorScheme from "./hooks/useColorScheme"
 import Navigation from "./navigation"
-import Temperature from "./components/Temperature"
 
 const GRAPHQL_ENDPOINT = "https://graphql-weather-api.herokuapp.com/"
 
