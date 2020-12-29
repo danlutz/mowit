e33f98a9ca291907b0478f77dc65e78e74ece64a
Michi Moesl <michael.moesl@me.com>
2020-12-29 18:00:10 +0100

    chore: add types for navigationStack

---
+++ packages/app/constants/types.ts
@@ -0,0 +1,22 @@
+export type AuthStackParameters = {
+	Onboarding: undefined
+	CreateAccount: undefined
+	ForgotPassword: undefined
+	SignIn: undefined
+	Verification: undefined
+}
+
+export type ProfileStackParameters = {
+	Profile: undefined
+	AlreadyRented: undefined
+	DataPrivacy: undefined
+	Settings: undefined
+	EditProfile: undefined
+	MyListings: undefined
+}
+
+export type TabStackParameters = {
+	Profile: undefined
+	Explore: undefined
+	AddListing: undefined
+}
