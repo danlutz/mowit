339ddb0f721d58f07416c08fea6e0c6a80d5f904
Michi Moesl <michael.moesl@me.com>
2020-12-30 13:21:11 +0100

    feat: add temperature to explore view

--- packages/app/components/Explore/CategoryView.tsx
+++ packages/app/components/Explore/CategoryView.tsx
@@ -1,13 +1,21 @@
 import React from "react"
-import { Alert, Button } from "react-native"
+import { Alert, Button, StyleSheet } from "react-native"
 import { ScreenContainer } from "react-native-screens"
+import Temperature from "../Temperature"
 import { Text } from "../Themed"
 
 export const CategoryView = () => {
 	return (
 		<ScreenContainer>
-			<Text>Category View Screen</Text>
+			<Text style={styles.placeholderText}>Category View Screen</Text>
+			<Text>Wetter Preview: {<Temperature />}</Text>
 			<Button title="OK" onPress={() => Alert.alert("OK")} />
 		</ScreenContainer>
 	)
 }
+
+const styles = StyleSheet.create({
+	placeholderText: {
+		paddingBottom: 30,
+	},
+})
