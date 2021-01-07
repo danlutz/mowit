8d7d9c09257169f62395916e74db811b43217336
Ennio Delorenzo <ennio.delorenzo@gmail.com>
2021-01-07 14:57:23 +0100

    feat(design): signIn / Auth Screens

--- packages/app/components/Auth/SignIn.tsx
+++ packages/app/components/Auth/SignIn.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Button, TextInput, StyleSheet, Alert } from "react-native"
+import { Button, TextInput, StyleSheet, Alert, View } from "react-native"
 import { ScreenContainer } from "react-native-screens"
 import { Text } from "../Themed"
 import { StackNavigationProp } from "@react-navigation/stack"
@@ -32,25 +32,32 @@ export const SignIn = ({ props }: Props) => {
 	}
 
 	return (
-		<ScreenContainer>
-			<Text>RENT.IT</Text>
+		<ScreenContainer style={styles.container}>
+			<Text style={styles.rentIT}>RENT.IT</Text>
 			<TextInput
-				style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+			style={styles.textfield}
 				onChangeText={(text) => oncChangeEmailText(text)}
 				value={eMailText}
 				keyboardType={"email-address"}
 				placeholder={"E-Mail Adresse"}
 			/>
 			<TextInput
+			style={styles.textfield}
 				secureTextEntry={true}
-				style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+
 				onChangeText={(text) => onChangePasswordText(text)}
 				value={passwordText}
 				placeholder={"Passwort"}
 			/>
-			<Button title="Anmelden" onPress={() => onlogin()} />
-			<Button title="Create Account In" onPress={() => navigation.push("CreateAccount")} />
-			<Button title="Passwort Vergessen" onPress={() => navigation.push("ForgotPassword")} />
+			<View style={styles.button}>
+			<Button color="#ffffff" title="Anmelden" onPress={() => onlogin()} />
+			</View>
+			<View style={styles.links}>
+			<Button color="#E67E22" title="Du hast noch kein Account? Jetzt registrieren" onPress={() => navigation.push("CreateAccount")} />
+			</View>
+			<View style={styles.linkszwei}>
+			<Button color="#E67E22" title="Passwort vergessen ?" onPress={() => navigation.push("ForgotPassword")} />
+			</View>
 		</ScreenContainer>
 	)
 }
@@ -59,4 +66,54 @@ interface Props {
 	props: StackNavigationProp<OnboardingAuthStackParameters>
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#FFF',
+		alignItems: "center",
+		paddingTop: 200,
+		fontFamily: 'Helvetica Neue'
+	},
+	rentIT: {
+		paddingTop: 10,
+		bottom: 80,
+		fontSize: 32,
+		paddingBottom: 20,
+		fontWeight: "bold",
+		textAlign: 'center'
+	},
+
+	textfield: {
+		height: 40, 
+		width: 340,
+		marginBottom: 10,
+		borderColor: '#F7F9FC', 
+		borderWidth: 1,
+		color: '#F7F9FC',
+		backgroundColor: '#F7F9FC'
+	},
+	links: {
+		marginTop: 30,
+		
+
+	},
+	linkszwei: {
+		marginBottom: 30,
+		
+
+	},
+	button: {
+		marginTop: 30,
+		backgroundColor: '#E67E22',
+		borderRadius: 4,
+		color: '#FFFFFF',
+		shadowColor: '#111111',
+		shadowOpacity: 0.2,
+		textTransform: 'uppercase',
+		width: 220,
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		}
+	},
+})
--- packages/app/screens/AuthScreens.tsx
+++ packages/app/screens/AuthScreens.tsx
@@ -10,6 +10,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center",
+		backgroundColor: "#FFF"
 	},
 	button: {
 		paddingHorizontal: 20,
