0c8dc93fd689e1a96674ae78e615c33de44e594c
Michi Moesl <michael.moesl@me.com>
2021-01-07 13:16:53 +0100

    feat(app): add fake Login function & input validation

--- packages/app/components/Auth/SignIn.tsx
+++ packages/app/components/Auth/SignIn.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Button, TextInput, StyleSheet } from "react-native"
+import { Button, TextInput, StyleSheet, Alert } from "react-native"
 import { ScreenContainer } from "react-native-screens"
 import { Text } from "../Themed"
 import { StackNavigationProp } from "@react-navigation/stack"
@@ -10,8 +10,26 @@ import { useNavigation } from "@react-navigation/native"
 export const SignIn = ({ props }: Props) => {
 	const { signIn } = React.useContext(AuthContext)
 	const navigation = useNavigation()
-	const [eMailText, oncChangeEmailText] = React.useState("E-Mail")
-	const [passwordText, onChangePasswordText] = React.useState("Password")
+	const [eMailText, oncChangeEmailText] = React.useState("")
+	const [passwordText, onChangePasswordText] = React.useState("")
+
+	//MARK: FAKE USER VALIDATION
+	const onlogin = () => {
+		if (eMailText == "" || passwordText == "") {
+			Alert.alert("E-Mail Adresse und Passwort darf nicht leer sein")
+		} else {
+			if (
+				(eMailText == "michi@rentit.com" || eMailText == "Michi@rentit.com") &&
+				passwordText == "123456"
+			) {
+				signIn()
+			} else {
+				Alert.alert("E-Mail Adresse oder Passwort falsch")
+			}
+		}
+
+		console.log(`email: ${eMailText} - password ${passwordText}`)
+	}
 
 	return (
 		<ScreenContainer>
@@ -21,14 +39,16 @@ export const SignIn = ({ props }: Props) => {
 				onChangeText={(text) => oncChangeEmailText(text)}
 				value={eMailText}
 				keyboardType={"email-address"}
+				placeholder={"E-Mail Adresse"}
 			/>
 			<TextInput
 				secureTextEntry={true}
 				style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
 				onChangeText={(text) => onChangePasswordText(text)}
 				value={passwordText}
+				placeholder={"Passwort"}
 			/>
-			<Button title="Anmelden" onPress={() => signIn()} />
+			<Button title="Anmelden" onPress={() => onlogin()} />
 			<Button title="Create Account In" onPress={() => navigation.push("CreateAccount")} />
 			<Button title="Passwort Vergessen" onPress={() => navigation.push("ForgotPassword")} />
 		</ScreenContainer>
