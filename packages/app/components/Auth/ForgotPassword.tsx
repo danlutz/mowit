ba405ffe7b3245bc5b1b8cc3b3241c1143dca69b
Ennio Delorenzo <ennio.delorenzo@gmail.com>
2021-01-07 14:58:11 +0100

    feat(design): forgotPasswort Screen

--- packages/app/components/Auth/ForgotPassword.tsx
+++ packages/app/components/Auth/ForgotPassword.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Alert, Button, TextInput } from "react-native"
+import {  Alert, Button, TextInput, StyleSheet } from "react-native"
 import { ScreenContainer } from "react-native-screens"
 import { View, Text } from "../Themed"
 
@@ -7,15 +7,68 @@ export const ForgotPassword = () => {
 	const [eMailText, oncChangeEmailText] = React.useState("E-Mail")
 
 	return (
-		<ScreenContainer>
-			<Text>ForgotPassword Screen</Text>
+		<ScreenContainer style={styles.container}>
+			<Text style={styles.rentIT}>RENT.IT</Text>
 			<TextInput
-				style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+			style={styles.textfield}
 				onChangeText={(text) => oncChangeEmailText(text)}
 				value={eMailText}
 				keyboardType={"email-address"}
+				placeholder={"E-Mail Adresse"}
 			/>
-			<Button title="Zurücksetzen" onPress={() => Alert.alert("Forgot Password clicked")} />
+			<View style={styles.button}>
+			<Button color="#fff" title="ZURÜCKSETZEN" onPress={() => Alert.alert("Du solltest in Kürze eine Email erhalten um dein Kennwort zurückzusetzen")} />
+			</View>
 		</ScreenContainer>
 	)
 }
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#FFF',
+		alignItems: "center",
+		paddingTop: 200,
+		fontFamily: 'Helvetica Neue'
+	},
+	rentIT: {
+		paddingTop: 10,
+		bottom: 80,
+		fontSize: 32,
+		paddingBottom: 20,
+		fontWeight: "bold",
+		textAlign: 'center'
+	},
+
+	textfield: {
+		height: 40, 
+		width: 340,
+		marginBottom: 10,
+		borderColor: '#F7F9FC', 
+		borderWidth: 1,
+		color: '#F7F9FC',
+		backgroundColor: '#F7F9FC'
+	},
+	links: {
+		marginTop: 30,
+		
+
+	},
+	linkszwei: {
+		marginBottom: 30,
+		
+
+	},
+	button: {
+		marginTop: 30,
+		backgroundColor: '#E67E22',
+		borderRadius: 4,
+		color: '#FFFFFF',
+		shadowColor: '#111111',
+		shadowOpacity: 0.2,
+		width: 220,
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		}
+	},
+})
