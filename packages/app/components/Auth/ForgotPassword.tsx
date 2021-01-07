24a8ff8a5d8f32d53981969a5c2a8395e7e433ee
Michi Moesl <michael.moesl@me.com>
2021-01-07 15:39:58 +0100

    feat(app): add password forgotten logic & 2FA code validation

--- packages/app/components/Auth/ForgotPassword.tsx
+++ packages/app/components/Auth/ForgotPassword.tsx
@@ -1,23 +1,31 @@
 import React from "react"
-import {  Alert, Button, TextInput, StyleSheet } from "react-native"
+import { Alert, Button, TextInput, StyleSheet } from "react-native"
 import { ScreenContainer } from "react-native-screens"
 import { View, Text } from "../Themed"
 
 export const ForgotPassword = () => {
-	const [eMailText, oncChangeEmailText] = React.useState("E-Mail")
+	const [eMailText, oncChangeEmailText] = React.useState("")
 
 	return (
 		<ScreenContainer style={styles.container}>
 			<Text style={styles.rentIT}>RENT.IT</Text>
 			<TextInput
-			style={styles.textfield}
+				style={styles.textfield}
 				onChangeText={(text) => oncChangeEmailText(text)}
 				value={eMailText}
 				keyboardType={"email-address"}
 				placeholder={"E-Mail Adresse"}
 			/>
 			<View style={styles.button}>
-			<Button color="#fff" title="ZURÜCKSETZEN" onPress={() => Alert.alert("Du solltest in Kürze eine Email erhalten um dein Kennwort zurückzusetzen")} />
+				<Button
+					color="#fff"
+					title="ZURÜCKSETZEN"
+					onPress={() =>
+						Alert.alert(
+							"Du solltest in Kürze eine Email erhalten um dein Kennwort zurückzusetzen",
+						)
+					}
+				/>
 			</View>
 		</ScreenContainer>
 	)
@@ -25,10 +33,10 @@ export const ForgotPassword = () => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#FFF',
+		backgroundColor: "#FFF",
 		alignItems: "center",
 		paddingTop: 200,
-		fontFamily: 'Helvetica Neue'
+		fontFamily: "Helvetica Neue",
 	},
 	rentIT: {
 		paddingTop: 10,
@@ -36,39 +44,35 @@ const styles = StyleSheet.create({
 		fontSize: 32,
 		paddingBottom: 20,
 		fontWeight: "bold",
-		textAlign: 'center'
+		textAlign: "center",
 	},
 
 	textfield: {
-		height: 40, 
+		height: 40,
 		width: 340,
 		marginBottom: 10,
-		borderColor: '#F7F9FC', 
+		borderColor: "#F7F9FC",
 		borderWidth: 1,
-		color: '#F7F9FC',
-		backgroundColor: '#F7F9FC'
+		color: "#F7F9FC",
+		backgroundColor: "#F7F9FC",
 	},
 	links: {
 		marginTop: 30,
-		
-
 	},
 	linkszwei: {
 		marginBottom: 30,
-		
-
 	},
 	button: {
 		marginTop: 30,
-		backgroundColor: '#E67E22',
+		backgroundColor: "#E67E22",
 		borderRadius: 4,
-		color: '#FFFFFF',
-		shadowColor: '#111111',
+		color: "#FFFFFF",
+		shadowColor: "#111111",
 		shadowOpacity: 0.2,
 		width: 220,
 		shadowOffset: {
 			width: 0,
 			height: 2,
-		}
+		},
 	},
 })
--- packages/app/components/Auth/Verification.tsx
+++ packages/app/components/Auth/Verification.tsx
@@ -3,14 +3,51 @@ import { Alert, Button } from "react-native"
 import { ScreenContainer } from "react-native-screens"
 import { View, Text } from "../Themed"
 import { AuthContext } from "../../constants/context"
+import {
+	CodeField,
+	Cursor,
+	useBlurOnFulfill,
+	useClearByFocusCell,
+} from "react-native-confirmation-code-field"
+import Navigation from "../../navigation"
 
 export const Verification = () => {
 	const { emailVerified } = React.useContext(AuthContext)
 
+	const cellCount = 6
+	const [value, setValue] = React.useState("")
+	const ref = useBlurOnFulfill({ value, cellCount: cellCount })
+	const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+		value,
+		setValue,
+	})
+
+	const validateCode = () => {
+		if (value != "698274") {
+			Alert.alert("Der eingegebene Code ist falsch.")
+		} else {
+			emailVerified()
+		}
+	}
+
 	return (
 		<ScreenContainer>
-			<Text>Verification Screen</Text>
-			<Button title="Email Verified" onPress={() => emailVerified()} />
+			<Text>Underline example</Text>
+			<CodeField
+				ref={ref}
+				{...props}
+				value={value}
+				onChangeText={setValue}
+				cellCount={cellCount}
+				keyboardType="number-pad"
+				textContentType="oneTimeCode"
+				renderCell={({ index, symbol, isFocused }) => (
+					<View onLayout={getCellOnLayoutHandler(index)} key={index}>
+						<Text>{symbol || (isFocused ? <Cursor /> : null)}</Text>
+					</View>
+				)}
+			/>
+			<Button title="Email Verified" onPress={() => validateCode()} />
 		</ScreenContainer>
 	)
 }
