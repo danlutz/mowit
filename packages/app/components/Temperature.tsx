33da58a583097ba27ca303e58e1be084b5b20287
Alihan Ekiz <alihan.ekiz@outlook.de>
2021-01-23 18:59:54 +0100

    add design to temperature indicator

--- packages/app/components/Temperature.tsx
+++ packages/app/components/Temperature.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Text, View } from "./Themed"
-
+import { StyleSheet } from 'react-native';
+import { Icon } from "react-native-elements"
 import { useQuery } from "@apollo/client"
 import { GetCityByNameDocument } from "@rentit/api/graphql-weather-api/generated/graphql-weather-api"
 
@@ -24,11 +25,52 @@ const Temperature = () => {
 
 	const temperatureInCelsius = kelvinToCelsius(temperatureInKelvin)
 
-	return <View> 
-		<Text>
-		Temperature in Innsbruck is {(temperatureInCelsius.toFixed(1))} ° Celsius 
-		</Text>
-		</View>
+	return 	<View style={styles.container}> 
+				<View style={styles.row}>
+					<Icon
+						size={18}
+						name="location"
+						type="ionicon"
+						color="#000"
+						style={styles.icon}
+					></Icon>
+					<Text style={styles.text}>Innsbruck</Text>
+				</View>
+				<View style={styles.row}>
+					<Icon
+						size={18}
+						name="thermometer"
+						type="ionicon"
+						color="#000"
+						style={styles.icon}
+					></Icon>
+					<Text style={styles.text}>{(temperatureInCelsius.toFixed(1))} ° Celsius</Text>
+				</View>
+			</View>
 }
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#ffffff',
+		height: 50,
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	row: {
+		flex: 1,
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	icon: {
+		marginRight: 5
+	},
+	text: {
+		fontWeight: "500"
+	}
+});
 
 export default Temperature
